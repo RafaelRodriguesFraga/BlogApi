0bf1e9e5bb32857e58fd3710cdad448daedf28ef
Rafael Fraga <ti.rafaelfraga@gmail.com>
2023-05-12 13:27:04 -0300

    Add UseUrls again

--- Blog.Api/Program.cs
+++ Blog.Api/Program.cs
@@ -2,6 +2,8 @@ using Blog.Api;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.WebHost.UseUrls("http://localhost:5090");
+
 var startup = new Startup(builder.Configuration);
 startup.ConfigureServices(builder.Services);
 
