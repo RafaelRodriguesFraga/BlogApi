95b087b53c9d8788b37055b1e88b67341a7d4342
Rafael Fraga <ti.rafaelfraga@gmail.com>
2023-05-31 23:01:17 -0300

    Add PostResponseViewModel

---
+++ Blog.Application/ViewModels/PostResponseViewModel.cs
@@ -0,0 +1,26 @@
+﻿using Blog.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Application.ViewModels
+{
+    public class PostResponseViewModel
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string Meta { get; set; }
+        public string[] Tags { get; set; }
+        public string Slug { get; set; }
+        public ThumbnailViewModel Thumbnail { get; set; }
+
+        public static implicit operator Post(PostResponseViewModel viewModel)
+        {
+            return new Post(viewModel.Title, viewModel.Content, viewModel.Meta, viewModel.Tags);
+        }
+      
+    }
+}
