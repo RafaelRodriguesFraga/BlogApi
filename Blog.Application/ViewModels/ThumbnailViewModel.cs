5d60576fb8b0346a5752731526e3c183a1ff84af
Rafael Fraga <ti.rafaelfraga@gmail.com>
2023-06-12 22:33:40 -0300

    Add ThumbnailRequestViewModel

--- Blog.Application/ViewModels/ThumbnailViewModel.cs
+++ Blog.Application/ViewModels/ThumbnailViewModel.cs
@@ -1,8 +1,24 @@
-﻿namespace Blog.Application.ViewModels
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Blog.Application.ViewModels
 {
     public class ThumbnailViewModel
     {
+        public ThumbnailViewModel(string url, string publicId)
+        {
+            Url = url;
+            PublicId = publicId;
+        }
+
         public string Url { get; set; }
         public string PublicId { get; set; }
+    }
+
+    public class ThumbnailRequestViewModel
+    {     
+
+        public IFormFile File { get; set; }
+
+      
     }
 }
