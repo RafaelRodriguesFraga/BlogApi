f7ad97179884d802c29590632dc9d25cfab80816
Rafael Fraga <ti.rafaelfraga@gmail.com>
2023-09-12 13:56:05 -0300

    Add regex to special characters and diacritics

--- Blog.Shared/StringHelper.cs
+++ Blog.Shared/StringHelper.cs
@@ -1,12 +1,37 @@
-﻿namespace Blog.Shared
+﻿using System.Globalization;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Blog.Shared
 {
     public static class StringHelper
     {
         public static string GenerateSlug(string title)
         {
-            title = title.ToLower().Replace(" ", "-");
+            title = RemoveDiacritics(title);
+            title = title.Trim();
+            title = Regex.Replace(title, "[^a-zA-Z0-9- ]", "");
+            title = title.Replace(" ", "-");
+            title = Regex.Replace(title, "-{2,}", "-");
+            title = title.ToLower();
 
             return title;
+        }
+
+        public static string RemoveDiacritics(string text)
+        {
+            string normalized = text.Normalize(NormalizationForm.FormD);
+            StringBuilder sb = new StringBuilder();
+
+            foreach (char c in normalized)
+            {
+                if (CharUnicodeInfo.GetUnicodeCategory(c) != UnicodeCategory.NonSpacingMark)
+                {
+                    sb.Append(c);
+                }
+            }
+
+            return sb.ToString();
         }
     }
 }
